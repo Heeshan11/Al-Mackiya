37e75d0e6d63e2e0efee4a0fe0a3b71f731d07d7
Heeshan11 <hishan111196@gmail.com>
2024-08-19 19:56:44 +0530

    new api

--- frontend/src/features/auth/authServices.js
+++ frontend/src/features/auth/authServices.js
@@ -16,6 +16,9 @@ const register = async (userData) => {
 // Login user
 const login = async (userData) => {
   const response = await axios.post(API_URL + 'login', userData)
+  console.log(API_URL + 'login');
+  console.log(API_URL);
+  
     if (response.data) {
       localStorage.setItem('user', JSON.stringify(response.data))
     }
